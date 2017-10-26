546d2c9db4b3e42c6374e7e7669612e279568afe
phemy <phemy.smith@gmail.com>
2017-10-27 00:49:59 +0100

    all controllers working

--- server/controllers/recipesController.js
+++ server/controllers/recipesController.js
@@ -4,15 +4,14 @@ let recipes=recipe.recipes;
 class RecipeCrude{
 
    static	createRecipe(req,res){
+	   recipes.push({
+		   "name": req.body.name,
+		   "id": req.body.id,
+		   "recipe_details": req.body.recipe_details,
 
-			recipes.push({
-			title:req.body.title,
-			id:recipes.recipes.length,
-			main_characters: req.body.main_characters,
-			poster:req.body.poster,
-			hero_image: req.body.hero_image,
-	})
-	res.status(200).send(recipes.recipes);
+	   })
+
+	res.status(200).send(recipes);
 
 	}
 
@@ -22,24 +21,54 @@ class RecipeCrude{
 
 	static deleteRecipe(req,res){
 		const value = req.params.id;
-		recipes.map((recipe) => {
-			if(recipe.id == value){
-				recipes.splice(recipe,1)
+		// recipes.map((recipe) => {
+		// 	if(recipe.id == value){
+		// 		let arr = indexOf(recipe);
+		// 		recipes.splice(arr,1);
+		// 	}
+		// })
+		for(let key=0; key<recipes.length; key++){
+			if(recipes[key].id === value){
+				recipes.splice(key,1);
 			}
-		})
+		}
 		res.status(200).json(recipes)
 	}
 	static updateRecipe(req,res){
-		const value = req.params.id;
+		const value = req.params.recipeId;
 		recipes.map((recipe) => {
 			if(recipe.id == value){
-				console.log(value)
+				recipe.name=req.body.name;
+				recipe.recipe_details=req.body.recipe_details;
+				recipe.id=req.body.id;
 			}
 		})
-		res.status(201).send('user updated');
+		res.status(201).send(recipes);
 	}
 	static findRecipe(req,res){
 		res.status(201).send('user found');
+	}
+	static postReviews(req,res){
+		const value = req.params.recipeId;
+		recipes.map((recipe) => {
+			if(recipe.id == value){
+				recipe.reviews=req.body.reviews;
+			}
+		})
+		res.status(201).send(recipes);
+	}
+	static highVote(req,res){
+		let max=0;
+		let display= {};
+		recipes.map((recipe) => {
+			if((recipe.vote.upvote) > (max)){
+
+				max = recipe.vote.upvote;
+				display=recipe;
+			}
+
+		})
+		res.status(201).json(display);
 	}
 
 }
--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -8,5 +8,6 @@ router.get('/api/recipes', RecipeCrude.readRecipes);
 router.post('/api/recipes', RecipeCrude.createRecipe);
 router.delete('/api/recipes/:id', RecipeCrude.deleteRecipe)
 router.put('/api/recipes/:recipeId', RecipeCrude.updateRecipe);
-
+router.post('/api/recipes/:recipeId/reviews',RecipeCrude.postReviews)
+router.get('/api/recipes/vote', RecipeCrude.highVote);
 export default router;
