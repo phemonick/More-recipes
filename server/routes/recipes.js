52e56d2757852f4a433e32bff8f6376301c10aa2
phemy <phemy.smith@gmail.com>
2017-10-27 12:00:48 +0100

    added more test cases

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -4,10 +4,11 @@ import {RecipeCrude} from '../controllers';
 
 const router = express.Router();
 
-router.get('/api/recipes', RecipeCrude.readRecipes);
-router.post('/api/recipes', RecipeCrude.createRecipe);
-router.delete('/api/recipes/:id', RecipeCrude.deleteRecipe)
-router.put('/api/recipes/:recipeId', RecipeCrude.updateRecipe);
-router.post('/api/recipes/:recipeId/reviews',RecipeCrude.postReviews)
-router.get('/api/recipes/vote', RecipeCrude.highVote);
+router.get('/api/v1/recipes', RecipeCrude.readRecipes);
+router.post('/api/v1/recipes', RecipeCrude.createRecipe);
+router.delete('/api/v1/recipes/:id', RecipeCrude.deleteRecipe)
+router.put('/api/v1/recipes/:recipeId', RecipeCrude.updateRecipe);
+router.post('/api/v1/recipes/:recipeId/reviews',RecipeCrude.postReviews)
+router.get('/api/v1/recipes/vote', RecipeCrude.highVote);
+
 export default router;
--- server/tests/test.js
+++ server/tests/test.js
@@ -1,23 +1,78 @@
+
+import chai from 'chai';
 import supertest from 'supertest';
-import {assert} from 'chai';
-import {recipe} from '../routes';
+import validateFunction from '../middleware/validate';
+import app from '../index';
 
-const app = recipe.router;
+const expect = chai.expect;
+const request = supertest(app);
+let data = {};
+let updateData = {};
+const upVote = '1';
 
-describe('GET /api/recipes', () => {
-
-
-it('should return status code 200', (done) => {
-    request(app)
-    .get('/api/recipes')
-    .expect('content-Type', /json/)  
-    .expect(200)
-    .expect((res) => {
-        assert.equal(typeof(res.body), typeof{});
-    })
-    .end(done);
-    
-})
-})
-
+describe('API Endpoints testing', () => {
+ describe('Get all recipes in the application', () => {
+   beforeEach(() => {
+     data = {
+       id: 1,
+       name: 'Banana milk shake',
+       description: 'Probably the best milk shake you ever had in your entire life',
+       category: 'Dessert',
+       ingredients: ['Milk', 'Banana', 'Olive oil'],
+       instructions: ['Blend the banana properly', 'Filter and shake'],
+       upVote: 0,
+       downVote: 0,
+       favorite: 0,
+       reviews: [
+         {
+           review: '',
+         },
+       ],
+     };
+     updateData = {
+       name: 'Banana milk shake',
+       description: 'Probably the best milk shake you ever had in your entire life',
+       category: 'Dessert',
+       ingredients: ['Milk', 'Banana', 'Olive oil'],
+       instructions: ['Blend the banana properly', 'Filter and shake'],
+     };
+   });
+   it('Should get all the recipes in the application', () => {
+     request.get('/api/v1/recipes')
+       .end((err, res) => {
+         expect(res.status).to.equal(200);
+         expect(res).to.be.an('object');
+       });
+   });
+   it('Should create new recipe in the application', () => {
+     request.post('/api/v1/recipes')
+       .send(data)
+       .end((err, res) => {
+         expect(res.status).to.equal(201);
+         expect(res).to.be.an('object');
+       });
+   });
+   it('Should update the current recipe in the application', () => {
+     request.put('/api/v1/recipes/:recipeId')
+       .send(updateData)
+       .end((err, res) => {
+         expect(res.status).to.equal(201);
+         expect(res).to.be.an('object');
+       });
+   });
+   it('Should delete the recipe in the application', () => {
+     request.delete('/api/v1/recipes/:recipeId')
+       .end((err, res) => {
+         expect(res.status).to.equal(201);
+       });
+   });
+   it('Should upvote the current recipe', () => {
+     request.post('/api/v1/recipes/:recipeId/testVote')
+       .send(upVote)
+       .end((err, res) => {
+         expect(res.status).to.equal(201);
+       });
+   });
+ });
+});
 
