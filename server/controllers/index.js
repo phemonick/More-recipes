ad590aaa6e5c73becec52d08bb4d59eb70cd6371
phemy <phemy.smith@gmail.com>
2017-11-01 12:04:06 +0100

    added postReview controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 import RecipeCrude from './recipesController';
 import UserCrude from './userController';
+import Review from './reviewController'
 
 export {
+    Review,
     RecipeCrude,
     UserCrude,
 }
