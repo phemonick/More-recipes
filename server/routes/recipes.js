7a4c70935c5f1cd4c68dfb89535558043f2abaaa
phemy <phemy.smith@gmail.com>
2017-11-02 16:11:59 +0100

    refactored

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,18 +1,17 @@
 import express from 'express';
-import { RecipeCrude, Review, Favorite, Vote } from '../controllers';
+import { RecipeController, Review, Favorite, Vote } from '../controllers';
 import Auth from '../middleware/auth';
 
 const recipeRouter = express.Router();
 
-recipeRouter.post('/api/v1/recipes', Auth.verifyToken, RecipeCrude.createRecipe);
-recipeRouter.delete('/api/v1/recipes/:recipeId', Auth.verifyToken, RecipeCrude.deleteRecipes);
-recipeRouter.put('/api/v1/recipes/:recipeId', Auth.verifyToken, RecipeCrude.updateRecipe);
-recipeRouter.get('/api/v1/:recipeId', Auth.verifyToken, RecipeCrude.getRecipe);
-recipeRouter.get('/api/v1/recipes', Auth.verifyToken, RecipeCrude.getAllRecipe);
+recipeRouter.post('/api/v1/recipes', Auth.verifyToken, RecipeController.createRecipe);
+recipeRouter.delete('/api/v1/recipes/:recipeId', Auth.verifyToken, RecipeController.deleteRecipes);
+recipeRouter.put('/api/v1/recipes/:recipeId', Auth.verifyToken, RecipeController.updateRecipe);
+recipeRouter.get('/api/v1/:recipeId', Auth.verifyToken, RecipeController.getRecipe);
+recipeRouter.get('/api/v1/recipes', Auth.verifyToken, RecipeController.getAllRecipe);
 recipeRouter.post('/api/v1/recipes/recipeId/reviews', Auth.verifyToken, Review.postReview);
-recipeRouter.post('/api/v1/users/:userId/recipes', Auth.verifyToken, Favorite.addFavorite)
-recipeRouter.get('/api/v1/users/:userId/recipes', Auth.verifyToken, Favorite.getFavorite);
 recipeRouter.put('/api/:recipeId/upvote', Auth.verifyToken, Vote.upvote);
 recipeRouter.put('/api/:recipeId/downvote', Auth.verifyToken, Vote.downvote);
+recipeRouter.get('/api/v1/:recipeId/highestVote', Auth.verifyToken, Vote.highestVote);
 
 export default recipeRouter;
