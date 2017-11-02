49cb70dc52dc29bba2f5cc1092a7f4aba941d087
phemy <phemy.smith@gmail.com>
2017-11-02 21:25:23 +0100

    heroku push

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`../config/config.js`)[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize;
