361983896698576d4e24c3a38aab1ff0c341d089
phemy <phemy.smith@gmail.com>
2017-11-02 16:14:07 +0100

    refactored code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,5 @@
-import RecipeCrude from './recipesController';
-import UserCrude from './userController';
+import RecipeController from './recipesController';
+import UserController from './userController';
 import Review from './reviewController';
 import Favorite from './favoriteController';
 import Vote from './voteRoute';
@@ -7,7 +7,7 @@ import Vote from './voteRoute';
 export {
   Favorite,
   Review,
-  RecipeCrude,
-  UserCrude,
-  Vote
+  RecipeController,
+  UserController,
+  Vote,
 };
