99b4d5b7195fee9d7039ee0987e7c95fc14c8f56
phemy <phemy.smith@gmail.com>
2017-11-02 16:15:53 +0100

    added more logic to user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { UserCrude } from '../controllers';
-import testMidd from '../middleware/auth';
+import { Favorite, UserController } from '../controllers';
+import Auth from '../middleware/auth';
 
 const userRouter = express.Router();
 
-userRouter.get('/api/v1/users', UserCrude.getUser);
-userRouter.post('/api/v1/users/signup', UserCrude.createUser);
-userRouter.post('/api/v1/users/signin', UserCrude.signIn);
+userRouter.get('/api/v1/users', UserController.getUser);
+userRouter.post('/api/v1/users/signup', UserController.createUser);
+userRouter.post('/api/v1/users/signin', UserController.signIn);
+userRouter.post('/api/v1/users/:userId/recipes', Auth.verifyToken, Favorite.addFavorite);
+userRouter.get('/api/v1/users/:userId/recipes', Auth.verifyToken, Favorite.getFavorite);
 
 export default userRouter;
